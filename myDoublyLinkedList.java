5e775450394bd0df60583a7d0c111832b2770896
obipez <35505653+obipez@users.noreply.github.com>
2018-02-20 22:52:52 -0800

    Update myDoublyLinkedList.java

--- myDoublyLinkedList.java
+++ myDoublyLinkedList.java
@@ -227,3 +227,180 @@ public class myDoublyLinkedList<E> {
 		System.out.println();
 	}
 }
+Node new_node = new Node(new_data, index);public class myDoublyLinkedList<E> {
+	Node head;
+	Node tail;
+	
+	myDoublyLinkedList() {
+		head = new Node();
+		tail = new Node();
+		head.next = tail;
+		tail.prev = head;
+	}
+	
+	public class Node {
+		E data; 
+		Node next;
+		Node prev;
+
+		public Node() {
+			data = null;
+			next = null;
+			prev = null;
+		}
+
+
+		public Node(E newData, int index) {
+			Node current = head;
+			int counter = 1;
+			while(counter-1 != index) {
+				current = current.next;
+				counter++;
+			}
+			data = newData;
+			this.next = current.next;
+			current.next = this;
+		}
+	}
+
+	public void traverseForward() {
+		Node current = head.next;
+		while(current.next != null) {
+			System.out.println(current.data);
+			current = current.next;
+		}
+//		System.out.println(current.data);
+	}
+	
+	
+	public void traverseBackward() {
+		Node current = tail.prev;
+		while(current.prev.data != null) {
+			System.out.println(current.data);
+			current = current.prev;
+		}
+		System.out.println(current.data);
+	}
+
+	public void addFirst(E new_data) {
+		Node new_node = new Node();
+		new_node.data = new_data;
+
+		new_node.next = head.next;
+		head.next.prev = new_node;
+		head.next = new_node;
+		new_node.prev = head;
+	}
+
+	public void addThis(E new_data, int index) {
+		Node new_node = new Node(new_data, index);
+	}
+
+	public void addLast(E new_data) {
+		Node new_node = new Node();
+		new_node.data = new_data;
+		
+		new_node.prev = tail.prev;
+		tail.prev.next = new_node;
+		tail.prev = new_node;
+		new_node.next = tail;
+	}
+
+	public void removeFirst() {
+		if(head == null) {
+			return;
+		}
+		if (head != null) {
+			head = head.next;
+		}
+	}
+
+	public void removeLast() {
+		Node last = head;
+		Node penultimate = null;
+		while(last.next != null) {
+			penultimate = last;
+			last = last.next;
+		}
+		penultimate.next = null;
+	}
+
+	public void removeThis(int index) {
+		if(index < 0 || index > size()) {
+			return;
+		}
+		if(index == 0) {
+			removeFirst();
+		}
+		else if(index == size()) {
+			removeThis(index);
+		}
+		else {
+			Node temp = head, prev = null;
+			for(int i = 0; i < index -1; i++) {
+				temp = temp.next;
+			}
+			prev = temp.next;
+			temp.next = prev.next;
+			prev = null;
+
+		}
+	}
+
+	public void changeData(E item, int index) {
+		Node new_node = new Node(item, index);
+//		Node new_node = head;							
+		Node prev = null;								
+		while (new_node != null && index >= 0) {
+			index--;
+			prev = new_node;
+			new_node = new_node.next;
+		}												
+		if (prev != null)
+			prev.data = item;
+	}
+
+	public int size() {
+		if(head.next == null) {
+			return 0;
+		}
+		int counter = 1;
+		Node current = head.next;
+		while(current.next != null) {
+			counter++;
+			current = current.next;
+		}
+		return counter;
+	}
+
+	public static void main(String[] args) {
+		myDoublyLinkedList<String> whatever = new myDoublyLinkedList<String>();
+
+		whatever.addFirst("Molly");
+		whatever.addFirst("Bongos");
+		whatever.addFirst("Ivan");
+		whatever.addFirst("3");
+		whatever.addFirst("Yoyo");
+		whatever.addLast("Clarence");
+		whatever.traverseForward();
+		System.out.println();
+		
+		whatever.removeThis(2);
+		whatever.traverseForward();
+		System.out.println();
+		
+		whatever.addThis("Ryan", 3);
+		whatever.addLast("Timmy");
+		whatever.removeFirst();
+		whatever.traverseForward();
+		System.out.println();
+		
+//		whatever.changeData("Fishies", 3);
+		whatever.removeLast();
+		whatever.traverseForward();
+		System.out.println();
+		
+		whatever.traverseBackward();
+		System.out.println();
+	}
+}
